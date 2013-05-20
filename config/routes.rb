a12f57c33dd792eed024f9a7f8d204af66c3af85
Sara_RG <sararijogarcia@gmail.com>
2013-05-20 23:46:05 +0100

    Login Completado-Pagina Principal

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Blog::Application.routes.draw do
+  # get "home/index"
+
   devise_for :users
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
 
@@ -56,7 +58,7 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'posts#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130518150740) do
+ActiveRecord::Schema.define(:version => 20130520185603) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(:version => 20130518150740) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
+    t.string   "name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
